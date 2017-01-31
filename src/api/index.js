74832e7716871ac691add0453c2435c83c62f9f0
Jan Akerman <jakerman@scottlogic.com>
2017-01-31 17:12:50 +0000

    Add todo now works again
    Mock API methods were added to our API for add & toggle todos. The
    action creator for addTodo is now an async action creator (and a thunk).
    This action triggers an request to add a new todo to the 'database' and
    dispatches an ADD_TODO_SUCCESS event upon success.
    
    The byId & createList reducers have been updated to handle this action.
    The byId simply returns the new state with this new ID mapped to the new
    Todo.
    The createList reducer behaves differently depending on the list
    filter. If the filter is active or all, then the ID is added.

--- src/api/index.js
+++ src/api/index.js
@@ -25,7 +25,7 @@ const delay = (ms) =>
 export const fetchTodos = (filter) =>
   delay(500).then(() => {
 
-    if (Math.random() > 0.5) throw new Error('Boom!'); // Fail sometimes.
+    if (Math.random() < 0.01) throw new Error('Boom!'); // Fail sometimes.
 
     switch (filter) {
       case 'all':
@@ -37,4 +37,23 @@ export const fetchTodos = (filter) =>
       default:
         throw new Error(`Unknown filter: ${filter}`);
     }
+  });
+
+export const addTodo = (text) =>
+  delay(500).then(() => {
+    const todo = {
+      id: v4(),
+      text,
+      completed: false,
+    };
+
+    fakeDatabase.todos.push(todo);
+    return todo;
+  });
+
+const toggleTodo = (id) =>
+  delay(500).then(() => {
+    const todo = fakeDatabase.todos.find(t => t.id === id);
+    todo.completed = !todo.completed;
+    return todo;
   });
--- src/reducers/createList.js
+++ src/reducers/createList.js
@@ -1,14 +1,22 @@
 import { combineReducers } from 'redux';
 
+// TODO: We're maintaining the lists as state, can these arrays be something we calculate on the fly?
+// Probably goes against the whole idiom of Redux but I'm too tired to think - answer this question tomorrow!
+
 const createList = (filter) => {
   const ids =  (state = [], action) => {
-    if (action.filter !== filter) {
-      return state;
-    }
-
     switch (action.type) {
       case 'FETCH_TODOS_SUCCESS':
-        return action.response.map(todo => todo.id);
+        // If the filter hasn't changed, then don't update the list. Note: Not 100% sure why this is needed - premature optimisation?
+        // TODO: Answer the above tomorrow!
+        return filter === action.filter ?
+          action.response.map(todo => todo.id) :
+          state;
+      case 'ADD_TODO_SUCCESS':
+        // We only add the ID to active & all lists as no newly added todo is completed.
+        return filter !== 'completed' ?
+          [... state, action.response.id] :
+          state;
       default:
         return state;
     }
