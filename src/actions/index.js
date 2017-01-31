d9136e9e1ada8c9a21dbe02c36a9b3090e3c1d81
Jan Akerman <jakerman@scottlogic.com>
2017-01-31 15:25:38 +0000

    Dispatch of REQUEST_TODOS automatic upon fetch
    When fetching new todos, a REQUEST_TODOS action is
    automatically dispatched. This removes the concern of
    which actions are needed from the component, and pushes
    that into the fetchTodos action creator.
    
    We've also written our own thunk middleware that gives us
    a powerful way to create action creators that need to
    dispatch multiple actions during some async event.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import { v4 } from 'node-uuid';
 import * as api from '../api';
 
-export const requestTodos = (filter) => ({
+const requestTodos = (filter) => ({
   type: 'REQUEST_TODOS',
   filter,
 });
@@ -12,10 +12,19 @@ const receiveTodos = (filter, response) => ({
   response,
 });
 
-// Async action creator.
-export const fetchTodos = (filter) =>
+/* Async action creator.
+  Previously we returned a promise, which we've decorated dispatch to handle,
+  resulting in a dispatch call on the result. Now, because we want to cause two
+  actions to dispatch, we can return a function instead, which is injected with
+  dispatch. This allows us to make multiple dispatches within our action creator.
+
+  Such functions returned from other functions are called 'thunks'.
+*/
+export const fetchTodos = (filter) => (dispatch) => {
+  dispatch(requestTodos(filter));
   api.fetchTodos(filter)
-    .then(response => receiveTodos(filter, response));
+    .then(response => dispatch(receiveTodos(filter, response)));
+};
 
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,27 @@
 import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
 import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
+/*
+"A thunk is a subroutine that is create, often automatically, to assist a call to
+another subroutine. Thunks are primarily used to represent an additional calculation
+that a subroute needs to execute, or call a routine that does not support the usual
+calling mechanism."
+
+Here, we're falling into the usual calling mechanism case. The function we want
+calling doesn't want the action, it wants the dispatch function so it can dispatch
+actions in a different way that default.
+
+We're assuming if we're passed a function, then that wants the dispatch.
+*/
+const thunk = (store) => (next) => (action) => {
+  typeof action === 'function' ?
+    action(store.dispatch) : next(action);
+};
 
 const configureStore = () => {
 
-  const middlewares = [promise];
+  const middlewares = [thunk];
 
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger());
