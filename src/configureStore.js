ed6ce262f212ff603ae0a3fd7f901b9da0a26911
Jan Akerman <jakerman@scottlogic.com>
2017-01-31 12:07:30 +0000

    Decorations of dispatch are written in a middleware pattern
    Each function that decorates the dispatch function on store is added to
    an array. Then each middleware wraps the raw dispatch function in turn.
    
    The middleware functions themselves return a function matching the same
    signiture of the original function.

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,18 +1,17 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
 
-const addLoggingToDispatch = (store) => {
+const logger = (store) => (next) => {
   /* eslint-disable no-console */
-  const rawDispatch = store.dispatch;
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
 
   return (action) => {
     console.group(action.type);
     console.log('%c prev state', 'color: gray', store.getState());
     console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log('%c next state', 'color: green', store.getState());
     console.groupEnd(action.type);
     return returnValue;
@@ -20,26 +19,33 @@ const addLoggingToDispatch = (store) => {
   /* eslint-enable no-console */
 };
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
   }
+  return next(action);
+};
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice()
+  // Allows middleware to be specified in the order that the action propagates through the middlewares.
+    .reverse()
+    .forEach(middleware => store.dispatch = middleware(store)(store.dispatch));
 };
 
 const configureStore = () => {
   const store = createStore(todoApp);
 
+  const middlewares = [];
+
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
   // The order is important here. We want the promise to be resolved before we do our special logging.
-  store.dispatch = addPromiseSupportToDispatch(store);
+  middlewares.push(promise)
+
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
