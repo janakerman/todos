99080bf8ddb0e0b09ded0051aa35d506d014300d
Jan Akerman <jakerman@scottlogic.com>
2017-01-31 10:42:26 +0000

    Pulls async behaviour into action creator.
    The action creator fetches the list of todos from our API and then
    resolves that promise to an action object.
    
    The dispatch function has been wrapped again to handle promises

--- src/configureStore.js
+++ src/configureStore.js
@@ -20,12 +20,26 @@ const addLoggingToDispatch = (store) => {
   /* eslint-enable no-console */
 };
 
+const addPromiseSupportToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+
+  return (action) => {
+    if (typeof action.then === 'function') {
+      return action.then(rawDispatch);
+    }
+    return rawDispatch(action);
+  }
+};
+
 const configureStore = () => {
   const store = createStore(todoApp);
 
   if (process.env.NODE_ENV !== 'production') {
     store.dispatch = addLoggingToDispatch(store);
   }
+
+  // The order is important here. We want the promise to be resolved before we do our special logging.
+  store.dispatch = addPromiseSupportToDispatch(store);
 
   return store;
 };
