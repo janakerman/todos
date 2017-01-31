08e14aba84444bd6f694546dc8377390532a9318
Jan Akerman <jakerman@scottlogic.com>
2017-01-31 13:13:57 +0000

    Adds promise & logger libraries and uses applyMiddleware
    Promise & logger implementations have been removed in favour for
    third party libraries. The middleware is also handled by redux's
    applyMiddleware utility.
    
    We can pass middware to the create store function as the third param
    (or second if no persisted state is provided).

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,53 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-const logger = (store) => (next) => {
-  /* eslint-disable no-console */
-  if (!console.group) {
-    return next;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = next(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-  /* eslint-enable no-console */
-};
-
-const promise = (store) => (next) => (action) => {
-  if (typeof action.then === 'function') {
-    return action.then(next);
-  }
-  return next(action);
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice()
-  // Allows middleware to be specified in the order that the action propagates through the middlewares.
-    .reverse()
-    .forEach(middleware => store.dispatch = middleware(store)(store.dispatch));
-};
 
 const configureStore = () => {
-  const store = createStore(todoApp);
 
-  const middlewares = [];
+  const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
 
-  // The order is important here. We want the promise to be resolved before we do our special logging.
-  middlewares.push(promise)
-
-  wrapDispatchWithMiddlewares(store, middlewares);
-
-  return store;
+  return createStore(
+    todoApp,
+    applyMiddleware(...middlewares) // Second param here is an optional 'enhancer'.
+  );
 };
 
 export default configureStore;
